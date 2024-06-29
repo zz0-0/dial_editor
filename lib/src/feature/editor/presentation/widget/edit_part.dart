361225fa9d0d5efdbbb06fc65b3ac8d755888b33
zz0-0 <122845086+zz0-0@users.noreply.github.com>
2024-06-30 11:42:17 +1200

    add selection area

--- lib/src/feature/editor/presentation/widget/edit_part.dart
+++ lib/src/feature/editor/presentation/widget/edit_part.dart
@@ -37,7 +37,6 @@ class _EditPartState extends ConsumerState<EditPart> {
     document = DocumentCodec(context).encode(fileString);
     nodes = document.children;
     markdownWidgetList = MarkdownRender().renderList(nodes);
-    print(markdownWidgetList);
   }
 
   @override
@@ -47,62 +46,102 @@ class _EditPartState extends ConsumerState<EditPart> {
     super.dispose();
   }
 
+  double _getTextHeight(TextStyle textStyle, String text) {
+    final textPainter = TextPainter(
+      text: TextSpan(text: text, style: textStyle),
+      textDirection: TextDirection.ltr,
+    )..layout();
+    return textPainter.height;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: markdownWidgetList.length,
-      itemBuilder: (context, index) {
-        return Row(
-          children: [
-            SizedBox(
-              width: 30,
-              child: Text("${index + 1}"),
-            ),
-            Expanded(
-              child: index == editingLineIndex
-                  ? TextField(
-                      controller: controller,
-                      focusNode: focusNode,
-                      style: currentTextStyle,
-                      cursorColor: Colors.blue,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Colors.transparent,
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0),
+          child: SizedBox(
+            width: 30,
+            child: ListView.builder(
+              itemCount: markdownWidgetList.length,
+              itemBuilder: (context, index) {
+                final textStyle = (nodes[index].style ??
+                    Theme.of(context).textTheme.bodyMedium)!;
+                final lineHeight = _getTextHeight(textStyle, nodes[index].text);
+                return Row(
+                  children: [
+                    const Spacer(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: lineHeight == 19.0 ? 0.5 : 0),
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        height: lineHeight,
+                        child: Text(
+                          "${index + 1}",
+                        ),
                       ),
-                      onEditingComplete: () {
-                        setState(() {
-                          nodes[editingLineIndex].updateText(controller.text);
-                          markdownWidgetList[editingLineIndex] =
-                              MarkdownRender().render(nodes[editingLineIndex]);
-                          fileString = document.toString();
-                          widget.file.writeAsStringSync(fileString);
-                          editingLineIndex = -1;
-                          controller.clear();
-                          currentTextStyle = const TextStyle();
-                        });
-                      },
-                    )
-                  : GestureDetector(
-                      onTapUp: (details) {
-                        setState(() {
-                          editingLineIndex = index;
-                          controller.text = nodes[index].rawText;
-                          controller.selection = TextSelection.collapsed(
-                            offset: controller.text.length,
-                          );
-                          currentTextStyle = nodes[index].style!;
-                        });
-                        WidgetsBinding.instance
-                            .addPostFrameCallback((timeStamp) {
-                          focusNode.requestFocus();
-                        });
-                      },
-                      child: markdownWidgetList[index],
                     ),
+                  ],
+                );
+              },
             ),
-          ],
-        );
-      },
+          ),
+        ),
+        Expanded(
+          child: SelectionArea(
+            child: ListView.builder(
+              itemCount: markdownWidgetList.length,
+              itemBuilder: (context, index) {
+                return index == editingLineIndex
+                    ? EditableText(
+                        controller: controller,
+                        focusNode: focusNode,
+                        style: currentTextStyle,
+                        cursorColor: Colors.blue,
+                        backgroundCursorColor: Colors.white,
+                        // decoration: const InputDecoration(
+                        //   border: InputBorder.none,
+                        //   fillColor: Colors.transparent,
+                        // ),
+                        onEditingComplete: () {
+                          setState(() {
+                            nodes[editingLineIndex].updateText(controller.text);
+                            markdownWidgetList[editingLineIndex] =
+                                MarkdownRender()
+                                    .render(nodes[editingLineIndex]);
+                            fileString = document.toString();
+                            widget.file.writeAsStringSync(fileString);
+                            editingLineIndex = -1;
+                            controller.clear();
+                            currentTextStyle = const TextStyle();
+                          });
+                        },
+                      )
+                    : GestureDetector(
+                        onTapUp: (details) {
+                          setState(() {
+                            controller.clear();
+                            editingLineIndex = index;
+                            controller.text = nodes[index].rawText;
+                            controller.selection = TextSelection.collapsed(
+                              offset: controller.text.length,
+                            );
+                            currentTextStyle =
+                                nodes[index].style ?? const TextStyle();
+                          });
+                          WidgetsBinding.instance
+                              .addPostFrameCallback((timeStamp) {
+                            focusNode.requestFocus();
+                          });
+                        },
+                        child: markdownWidgetList[index],
+                      );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
